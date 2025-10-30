ef6d407308caf1e658ddaaec297686e28abb1d53
Freark Bergstra <514721@student.firda.nl>
2025-10-30 13:30:29 +0100

    Create cli.py

---
+++ cli-adventure/cli.py
@@ -0,0 +1,18 @@
+import sys
+from time import sleep
+# Geeft een verhaaltje en vervolgens twee keuzes aan de gebruiker.
+
+print()
+sleep(0.5)
+print("Je bent eindelijk klaar met school na een lange dag van 8:30 tot 17:15 en moet wachten bij de bushalte op de bus naar huis, je staat er net 5 minuten en ziet een groen potje staan bij de lantaarnpaal, je bent aan het nadenken wat je zou moeten doen met dit potje.")
+print()
+sleep(0.5)
+sleep(0.5)
+# Geeft de gebruiker 2 keuzes
+print("A: Je pakt het potje op en opent het potje en kijkt wat erin zit.")
+sleep(0.5)
+print("B: Je laat het potje staan en wacht op de bus.")
+sleep(0.5)
+print()
+sleep(0.5)
+antwoord = input("Maak een keuze (A of B): ")
